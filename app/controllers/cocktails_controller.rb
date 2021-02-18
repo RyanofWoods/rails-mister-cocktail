3fe4e9653bf0ac8759bd076b903dd6f1bc0ce132
Ryan Woods <contact@ryanofwoods.com>
2021-02-18 15:18:29 +0100

    Added Cocktail #new and #create as well as new page and form partial

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -3,4 +3,24 @@ class CocktailsController < ApplicationController
   def index
     @cocktails = Cocktail.all
   end
+
+  def new
+    @cocktail = Cocktail.new
+  end
+
+  def create
+    @cocktail = Cocktail.new(cocktail_params)
+
+    if @cocktail.save
+      # redirect later when path is available
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def cocktail_params
+    params.require(:cocktail).permit(:name)
+  end
 end
