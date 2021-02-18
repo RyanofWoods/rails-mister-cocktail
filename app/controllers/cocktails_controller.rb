3258f3b27ce22f3021cab3c309bc4fbd731ab333
Ryan Woods <contact@ryanofwoods.com>
2021-02-18 17:29:41 +0100

    Added destroy for dose

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -17,7 +17,7 @@ class CocktailsController < ApplicationController
     @cocktail = Cocktail.new(cocktail_params)
 
     if @cocktail.save
-      # redirect later when path is available
+      redirect_to cocktail_path(@cocktail)
     else
       render :new
     end
--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,6 +1,7 @@
 class DosesController < ApplicationController
 
   before_action :set_cocktail, only: [:new, :create]
+  before_action :set_dose, only: [:destroy]
 
   def new
     @dose = Dose.new
@@ -17,10 +18,19 @@ class DosesController < ApplicationController
     end
   end
 
+  def destroy
+    @dose.destroy
+    redirect_to cocktail_path(@dose.cocktail)
+  end
+
   private
 
   def dose_params
     params.require(:dose).permit(:description, :ingredient_id)
+  end
+
+  def set_dose
+    @dose = Dose.find(params[:id])
   end
 
   def set_cocktail
