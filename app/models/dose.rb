0533a0f1a07ec2d442d9a771b9fcd88d77b3f6a9
Ryan Woods <contact@ryanofwoods.com>
2021-02-18 13:38:24 +0100

    Added validations for Models: Cocktail, Dose and Ingredient

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -1,4 +1,7 @@
 class Dose < ApplicationRecord
   belongs_to :cocktail
   belongs_to :ingredient
+  validates :description, presence: true
+
+  validates :ingredient_id, uniqueness: { scope: :cocktail_id, message: "Can only have an ingredient once in a cocktail" }
 end
