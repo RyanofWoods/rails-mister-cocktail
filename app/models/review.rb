e29dd4115280a0e50abfb060c172f524448f6ebf
Ryan Woods <contact@ryanofwoods.com>
2021-02-19 17:00:26 +0100

    Created validations and associations for Review

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,9 @@
 class Review < ApplicationRecord
-  belongs_to :cocktail
+  RATING = [0, 1, 2, 3, 4, 5]
+
+  validates :content, :rating, :cocktail_id, presence: true
+  validates :rating, numericality: { only_integer: true }
+  validates :rating, inclusion: { in: RATING }
+
+  belongs_to :cocktail, dependent: :destroy
 end
